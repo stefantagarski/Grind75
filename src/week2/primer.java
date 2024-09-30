e9a05b488557b5b6a10d9533da13b6ffe0409508
Stefan Tagarski <stefantagarski@gmail.com>
2024-09-30 21:49:25 +0200

    example

---
+++ src/week2/primer.java
@@ -0,0 +1,13 @@
+package week2;
+
+import java.util.Scanner;
+
+public class primer {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int x = scanner.nextInt();
+        if (x > 3) {
+            System.out.println(x + " is greater than 3");
+        }
+    }
+}
